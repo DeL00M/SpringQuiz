a7e68c1f1f45b8c991a9e7f236d2628f8080ca84
DeL00M <del00m@mail.ru>
2020-10-02 15:10:56 +0300

    Initial commit

---
+++ src/main/java/ru/del00m/SpringQuiz/SpringQuizApplication.java
@@ -0,0 +1,13 @@
+package ru.del00m.SpringQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuizApplication.class, args);
+	}
+
+}
