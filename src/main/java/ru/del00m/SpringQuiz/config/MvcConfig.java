a441f33bcf68510837dc4deecf88be66a5151dac
del00m <del00m@mail.ru>
2020-10-07 17:21:24 +0300

    Delete resource path

--- src/main/java/ru/del00m/SpringQuiz/config/MvcConfig.java
+++ src/main/java/ru/del00m/SpringQuiz/config/MvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -18,7 +19,6 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String path = "file://" + new File("").getAbsolutePath() + staticDirPath + imgUploadDir;
-        registry.addResourceHandler("/img/**").addResourceLocations(path);
+
     }
 }
