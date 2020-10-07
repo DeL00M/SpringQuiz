bccb52975c02da99d654c0eda7d0bebd2540a8ec
DeL00M <del00m@mail.ru>
2020-10-07 17:20:28 +0300

    Fixed resource path to handle all platform

--- src/main/java/ru/del00m/SpringQuiz/config/MvcConfig.java
+++ src/main/java/ru/del00m/SpringQuiz/config/MvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -18,7 +19,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String path = "file://" + new File("").getAbsolutePath() + staticDirPath + imgUploadDir;
-        registry.addResourceHandler("/img/**").addResourceLocations(path);
+        String path = "file:" + File.separator + File.separator + new File("").getAbsolutePath() + Paths.get(staticDirPath + imgUploadDir);
+        registry.addResourceHandler(Paths.get(imgUploadDir) + "**").addResourceLocations(path);
     }
 }
